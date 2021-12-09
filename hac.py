7d5fb08a8291510303b0cc6e2ba96faaca4c60d2
oguz <oguzkaganaltas@gmail.com>
2021-12-09 15:28:55 +0200

    init

---
+++ hac.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+def single_linkage(c1, c2):
+    """
+    Given clusters c1 and c2, calculates the single linkage criterion.
+    :param c1: An (N, D) shaped numpy array containing the data points in cluster c1.
+    :param c2: An (M, D) shaped numpy array containing the data points in cluster c2.
+    :return: A float. The result of the calculation.
+    """
+
+
+def complete_linkage(c1, c2):
+    """
+    Given clusters c1 and c2, calculates the complete linkage criterion.
+    :param c1: An (N, D) shaped numpy array containing the data points in cluster c1.
+    :param c2: An (M, D) shaped numpy array containing the data points in cluster c2.
+    :return: A float. The result of the calculation.
+    """
+
+
+def average_linkage(c1, c2):
+    """
+    Given clusters c1 and c2, calculates the average linkage criterion.
+    :param c1: An (N, D) shaped numpy array containing the data points in cluster c1.
+    :param c2: An (M, D) shaped numpy array containing the data points in cluster c2.
+    :return: A float. The result of the calculation.
+    """
+
+
+def centroid_linkage(c1, c2):
+    """
+    Given clusters c1 and c2, calculates the centroid linkage criterion.
+    :param c1: An (N, D) shaped numpy array containing the data points in cluster c1.
+    :param c2: An (M, D) shaped numpy array containing the data points in cluster c2.
+    :return: A float. The result of the calculation.
+    """
+
+
+def hac(data, criterion, stop_length):
+    """
+    Applies hierarchical agglomerative clustering algorithm with the given criterion on the data
+    until the number of clusters reaches the stop_length.
+    :param data: An (N, D) shaped numpy array containing all of the data points.
+    :param criterion: A function. It can be single_linkage, complete_linkage, average_linkage, or
+    centroid_linkage
+    :param stop_length: An integer. The length at which the algorithm stops.
+    :return: A list of numpy arrays with length stop_length. Each item in the list is a cluster
+    and a (Ni, D) sized numpy array.
+    """
---
+++ kmeans.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+def assign_clusters(data, cluster_centers):
+    """
+    Assigns every data point to its closest (in terms of Euclidean distance) cluster center.
+    :param data: An (N, D) shaped numpy array where N is the number of examples
+    and D is the dimension of the data
+    :param cluster_centers: A (K, D) shaped numpy array where K is the number of clusters
+    and D is the dimension of the data
+    :return: An (N, ) shaped numpy array. At its index i, the index of the closest center
+    resides to the ith data point.
+    """
+
+
+def calculate_cluster_centers(data, assignments, cluster_centers, k):
+    """
+    Calculates cluster_centers such that their squared Euclidean distance to the data assigned to
+    them will be lowest.
+    If none of the data points belongs to some cluster center, then assign it to its previous value.
+    :param data: An (N, D) shaped numpy array where N is the number of examples
+    and D is the dimension of the data
+    :param assignments: An (N, ) shaped numpy array with integers inside. They represent the cluster index
+    every data assigned to.
+    :param cluster_centers: A (K, D) shaped numpy array where K is the number of clusters
+    and D is the dimension of the data
+    :param k: Number of clusters
+    :return: A (K, D) shaped numpy array that contains the newly calculated cluster centers.
+    """
+
+
+def kmeans(data, initial_cluster_centers):
+    """
+    Applies k-means algorithm.
+    :param data: An (N, D) shaped numpy array where N is the number of examples
+    and D is the dimension of the data
+    :param initial_cluster_centers: A (K, D) shaped numpy array where K is the number of clusters
+    and D is the dimension of the data
+    :return: cluster_centers, objective_function
+    cluster_center.shape is (K, D).
+    objective function is a float. It is calculated by summing the squared euclidean distance between
+    data points and their cluster centers.
+    """
