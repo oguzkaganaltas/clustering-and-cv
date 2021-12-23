eae653d727a8c78a5088bd6e75fa0397dc7f9f84
oguz <oguzkaganaltas@gmail.com>
2021-12-23 09:57:28 +0200

    indexing fixed

--- run_knn.py
+++ run_knn.py
@@ -19,19 +19,23 @@ knn_results_l1=[]
 for k in range(1,179):
     knn_results_l1.append(cross_validation(train_set,train_labels,10,k,"L1")) 
 
+best_k_l2 = np.argmax(knn_results_l2) + 1 # +1 is to convert index to number
+best_k_l1 = np.argmax(knn_results_l1) + 1 # +1 is to convert index to number
 plt.plot(knn_results_l2)
-plt.text(0.5, 0.5, "best k value: "+str(np.argmax(knn_results_l2)))
+plt.text(0.5, 0.5, "best k value: "+str(best_k_l2))
 plt.xlabel("k values")
 plt.ylabel("accuracy %")
 plt.title("L2 norm")
+plt.show()
 
-plt.plot(knn_results_l1,label="best k value: "+str(np.argmax(knn_results_l1)))
-plt.text(0.5, 0.5, "best k value: "+str(np.argmax(knn_results_l1)))
+plt.plot(knn_results_l1)
+plt.text(0.5, 0.5, "best k value: "+str(best_k_l1))
 plt.xlabel("k values")
 plt.ylabel("accuracy %")
 plt.title("L1 norm")
+plt.show()
 
-print(cross_validation(test_set,test_labels,10,12,"L1"))
+print(cross_validation(test_set,test_labels,10,best_k_l1-1,"L1"))# -1 is for to number to index conversion
 
 
-print(cross_validation(test_set,test_labels,10,12,"L2"))
+print(cross_validation(test_set,test_labels,10,best_k_l2-1,"L2"))# -1 is for to number to index conversion
